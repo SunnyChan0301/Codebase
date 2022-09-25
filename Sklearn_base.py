518e060bba774e085a9f0d1ba20bcc4d459a8a44
SunnyChan0301 <92528621+SunnyChan0301@users.noreply.github.com>
2022-09-26 00:51:16 +0800

    Create Sklearn_base.py

---
+++ Sklearn_base.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score,mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+
+X,y=datasets.fetch_california_housing(return_X_y=True,as_frame=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+####### Random Forest Regressor
+rf = RandomForestRegressor(random_state=0)
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+print(f'Mean absolute error: {mae:.2f}')
+print(f'Mean squared error: {mse:.2f}')
+print(f'Root mean squared error: {rmse:.2f}')
+# Feature importance
+feature_names = X.columns
+model_coefficients = rf.feature_importances_
+
+coefficients_df = pd.DataFrame(data = model_coefficients, 
+                              index = feature_names, 
+                              columns = ['Coefficient value']
+                              ).reset_index().sort_values('Coefficient value',ascending=False)
+coefficients_df
+
+####### Linear regression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+print(f'Mean absolute error: {mae:.2f}')
+print(f'Mean squared error: {mse:.2f}')
+print(f'Root mean squared error: {rmse:.2f}')
+# coefficient
+feature_names = X.columns
+model_coefficients = regressor.coef_.round(decimals=3)
+
+coefficients_df = pd.DataFrame(data = model_coefficients, 
+                              index = feature_names, 
+                              columns = ['Coefficient value']
+                              )
+print(coefficients_df)
+
+###### Decision tree
+y_pred = clf.predict(X_test)
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+print(f'Mean absolute error: {mae:.2f}')
+print(f'Mean squared error: {mse:.2f}')
+print(f'Root mean squared error: {rmse:.2f}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
