6ca6b377d3cdc5a5a40cf990918d29067d6e3c92
SunnyChan0301 <92528621+SunnyChan0301@users.noreply.github.com>
2022-09-26 09:22:47 +0800

    Update Sklearn_smt.py

--- Sklearn_smt.py
+++ Sklearn_smt.py
@@ -46,4 +46,18 @@ print('accuracy: ' + str(accuracy_percentage))
 print('recall: ' + str(recall_pen))
 print('precision: ' + str(precision_pen))
 
+## AUC
+import numpy as np
+from sklearn import metrics
+y = np.array([1, 1, 2, 2])
+pred = np.array([0.1, 0.4, 0.35, 0.8])
+fpr, tpr, thresholds = metrics.roc_curve(y, pred, pos_label=2)
+metrics.auc(fpr, tpr)
+
 pd.DataFrame(sorted(zip(x.columns,clf.feature_importances_),key=lambda _: -_[1]))
+
+
+## Minmax scaler
+from sklearn.preprocessing import MinMaxScaler
+mc=MinMaxScaler()
+scaled_x=mc.fit_transform(x)
